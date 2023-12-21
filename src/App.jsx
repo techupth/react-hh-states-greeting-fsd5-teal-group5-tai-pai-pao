1b670551653a957d7f32127812c06555d49ca239
Pao <chaiwatt77@gmail.com>
2023-12-21 14:08:58 +0700

    HH1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,58 @@
 import "./App.css";
+import {useState} from "react";
 
 function App() {
+  const [greeting,setGreeting] = useState("Greeting Message");
+  const gg = ["สวัสดี!", "Hi!", "你好!"];
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greeting}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        {
+          gg.map((item)=>{
+            return (<button onClick = {()=>{
+              setGreeting(item)
+            }}>{item}</button>)
+          })
+        }
+
       </div>
     </div>
   );
 }
 
 export default App;
+
+/*
+import "./App.css";
+import {useState} from "react";
+
+function App() {
+  const [greeting,setGreeting] = useState("Greeting Message");
+  const gTH = "สวัสดี!";
+  const gEN = "Hi!";
+  const gCN = "你好!";
+  return (
+    <div className="App">
+      <div className="greeting-container">{greeting}</div>
+      <div className="buttons">
+        <button onClick = {()=>{
+          setGreeting(gTH);
+        }}>{gTH}</button>
+
+        <button onClick={()=>{
+          setGreeting(gEN)
+        }}>{gEN}</button>
+        
+        <button onClick={()=>{
+          setGreeting(gCN)
+        }}>{gCN}</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+*/
